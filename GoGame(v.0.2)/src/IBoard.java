95a9b5daa175eb3e188e4d2fd033f5079b07ad36
Kate Zabelova <kzabelova@gmail.com>
2015-04-29 16:02:14 +0300

    Delete

--- GoGame(v.0.2)/src/IBoard.java
+++
@@ -1,8 +0,0 @@
-public interface IBoard {
-    public final int cellsize = 28;
-    
-    void Undo();
-    void Redo();
-    boolean removeStone(Stone s);
-    
-}
--- Server(v.0.1)/src/MainClass.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author Saia
- */
-public class MainClass {
-    public static void main(String[] args) {
-        new Server();
-    }
-}
